35f860e88ecd2b898ee459f50890c3c325ce48de
Jared King <j@jaredtking.com>
2014-08-25 10:46:35 -0500

    improvements to console app

--- src/App/Console/MigrateCommand.php
+++ src/App/Console/MigrateCommand.php
@@ -35,8 +35,69 @@ class MigrateCommand extends Command
 
     protected function execute( InputInterface $input, OutputInterface $output )
     {
-        $result = $this->app->migrate( $input->getArgument( 'module' ), true );
+        $result = $this->migrate( $input->getArgument( 'module' ), $output );
 
         return $result ? 0 : 1;
+    }
+
+    /**
+     * Runs migrations for all app modules or a specified module.
+     * Also, will setup database sessions if enabled
+     *
+     * @param string $module optional module
+     * @param OutputInterface $output
+     *
+     * @return boolean success
+     */
+    private function migrate( $module = '', OutputInterface $output )
+    {
+        $success = true;
+
+        $output->writeln( '-- Running migrations' );
+
+        // database sessions
+        if( $this->app[ 'config' ]->get( 'sessions.adapter' ) == 'database' )
+        {
+            $output->writeln( 'Migrating Database Sessions' );
+
+            $result = Session\Database::install();
+
+            if( $result )
+                $output->writeln( '-- Database Sessions Installed' );
+            else
+                $output->writeln( '-- Error installing Database Sessions' );
+
+            $success = $result && $success;
+        }
+
+        // module migrations
+        $modules = (empty($module)) ? $this->app[ 'config' ]->get( 'modules.all' ) : [ $module ];
+
+        foreach( (array)$modules as $mod )
+        {
+            $output->writeln( "-- Migrating $mod" );
+
+            putenv( "PHINX_APP_MODULE=$mod" );
+            ob_start();
+            system( 'php vendor/robmorgan/phinx/bin/phinx migrate' );
+            $output = ob_get_contents();
+            ob_end_clean();
+
+            $lines = explode( "\n", $output );
+
+            // clean up the output
+            foreach( $lines as $line )
+            {
+                if( !empty( $line ) && substr( $line, 0, 3 ) == ' ==' )
+                    $output->writeln( $line );
+            }
+        }
+
+        if( $success )
+            $output->writeln( '-- Success!' );
+        else
+            $output->writeln( '-- Error running migrations' );
+
+        return $success;
     }
 }
