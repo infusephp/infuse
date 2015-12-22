32ac14bcbaf04ee439b38e26ca931147c91444f0
Jared King <j@jaredtking.com>
2015-12-22 14:04:56 -0600

    remove verbosity mode in test bootstrapping as it was not very helpful

--- src/Test.php
+++ src/Test.php
@@ -29,9 +29,7 @@ class Test implements PHPUnit_Framework_TestListener
      */
     public static $userPassword = 'testpassword';
 
-    private $verbose;
-
-    public function __construct($verbose = false)
+    public function __construct()
     {
         $config = @include 'config.php';
         if (!$config) {
@@ -44,14 +42,8 @@ class Test implements PHPUnit_Framework_TestListener
 
         ini_set('display_errors', 1);
 
-        $this->verbose = $verbose;
-
         /* Create a test user and sign in */
         if (class_exists('App\Users\Models\User')) {
-            if ($this->verbose) {
-                echo "Logging in a test user to run the test suite.\n";
-            }
-
             $user = new User();
             $testInfo = [
                 'user_email' => self::$userEmail,
@@ -73,14 +65,6 @@ class Test implements PHPUnit_Framework_TestListener
 
             $success = $user->create($testInfo);
 
-            if ($this->verbose) {
-                if ($success) {
-                    echo 'User #'.$user->id()." created.\n";
-                } else {
-                    echo "Could not create test user.\n";
-                }
-            }
-
             self::$app['user'] = new User($user->id(), true);
         }
 
@@ -88,87 +72,15 @@ class Test implements PHPUnit_Framework_TestListener
         self::$app['config']->set('email.type', 'nop');
     }
 
-    public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
-    {
-        if ($this->verbose) {
-            printf(" Error while running test '%s'.\n", $test->getName());
-        }
-    }
-
-    public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
-    {
-        if ($this->verbose) {
-            printf("Test '%s' failed.\n", $test->getName());
-        }
-    }
-
-    public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
-    {
-        if ($this->verbose) {
-            printf("Test '%s' is incomplete.\n", $test->getName());
-        }
-    }
-
-    public function addRiskyTest(PHPUnit_Framework_Test $test, Exception $e, $time)
-    {
-        if ($this->verbose) {
-            printf("Test '%s' is deemed risky.\n", $test->getName());
-        }
-    }
-
-    public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
-    {
-        if ($this->verbose) {
-            printf("Test '%s' has been skipped.\n", $test->getName());
-        }
-    }
-
     public function startTest(PHPUnit_Framework_Test $test)
     {
-        if ($this->verbose) {
-            printf("Test '%s' started.\n", $test->getName());
-        }
-
         if (class_exists('App\Users\Models\User')) {
             self::$app['user']->disableSU();
         }
     }
 
-    public function endTest(PHPUnit_Framework_Test $test, $time)
-    {
-        if ($this->verbose) {
-            printf("Test '%s' ended.\n", $test->getName());
-        }
-    }
-
     public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
     {
-        if ($this->verbose) {
-            printf("TestSuite '%s' started.\n", $suite->getName());
-        } elseif ($suite->getName() != 'App') {
-            printf("\n\n%s:\n", $suite->getName());
-        }
-    }
-
-    public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
-    {
-        if ($this->verbose) {
-            printf("TestSuite '%s' ended.\n", $suite->getName());
-        }
-
-        // nuke memcache in between test suites
-        if (self::$app['config']->get('memcache.enabled')) {
-            self::$app['memcache']->flush();
-        }
-
-        $errors = self::$app['errors']->errors();
-
-        if (count($errors) > 0) {
-            if ($this->verbose) {
-                printf("TestSuite '%s' produced these errors:\n", $suite->getName());
-                print_r($errors);
-            }
-            self::$app['errors']->clear();
-        }
+        printf("\n\n%s:\n", $suite->getName());
     }
 }
